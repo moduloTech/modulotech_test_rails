6490d900ed697e0c67c08cc2463658dc2ce13773
Eric Kuoch <ek.kuoch@gmail.com>
2023-03-14 00:55:10 +0100

    feat(models): add scope to books

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  scope :booked_between, -> (check_in, check_out) { where('checkin > ? AND checkout < ?', check_in, check_out) } 
+  scope :past, -> { where("checkout < :date_today", date_today: Date.today) } 
+  scope :current, -> { where("checkout >= :date_today", date_today: Date.today) } 
 end
