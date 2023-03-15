620b2317dcf5eae82d43075faddfb0420c4c7a02
Eric Kuoch <ek.kuoch@gmail.com>
2023-03-15 16:32:33 +0100

    fail(javascript): flatpickr does not work properly. to fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -11,15 +11,21 @@ popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
 });
 
-let flatpickrTriggerList = Array.from(document.querySelectorAll('[data-controller="flatpickr"]'));
-flatpickrTriggerList.map(function (ca) {
-    const end_date = document.querySelector('#end_date');
-    const totalPrice = document.querySelector('#totalprice');
-    const roomPrice = document.querySelector('#room-price').dataset.price;
-    const bookingForm = document.getElementById('booking-form-div');
-    const bookings = JSON.parse(bookingForm.dataset.bookings);
-    new flatpickr(ca, {
-            mode: "range",
-            dateFormat: "Y-m-d H:i",
-          })
-    });
+
+// TO FIX LATER
+    // your code here
+    let flatpickrTriggerList = Array.from(document.querySelectorAll('[data-controller="flatpickr"]'));
+    flatpickrTriggerList.map(function (ca) {
+        const end_date = document.querySelector('#end_date');
+        const totalPrice = document.querySelector('#totalprice');
+        const roomPrice = document.querySelector('#room-price').dataset.price;
+        const bookingForm = document.getElementById('booking-form-div');
+        const bookings = JSON.parse(bookingForm.dataset.bookings);
+        if(bookings) {
+            new flatpickr(ca, {
+                dateFormat: "Y-m-d H:i",
+                "disable": bookings
+                // more options available on the documentation!
+              })
+            }
+        });
--- app/javascript/controllers/init_flatpickr.js
+++ app/javascript/controllers/init_flatpickr.js
@@ -1,11 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
-import flatpickr from "flatpickr";
 
 export default class extends Controller {
   connect() {
-    console.log(1)
     new flatpickr(this.element, {
-      enableTime: true
       // more options available on the documentation!
     });
   }
