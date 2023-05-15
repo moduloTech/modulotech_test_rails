47e75cdf1fc9a3f6209de249fe6c3d9ddf6afaf6
Matthieu Ciappara <ciappa_m@modulotech.fr>
2023-05-15 15:14:10 +0200

    seed: Add two more users and two todos

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,8 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 User.find_or_create_by!(email: 'uno@test.test') { |u| u.password = '123456' }
+User.find_or_create_by!(email: 'duo@test.test') { |u| u.password = '123456' }
+User.find_or_create_by!(email: 'trio@test.test') { |u| u.password = '123456' }
+
+# TODO: Add a room owned by uno
+# TODO: Add a room owned by duo
