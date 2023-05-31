f796d727bb128dcb31c84a2c69ffa4c5540ea637
point212 <point212@gmail.com>
2023-05-31 18:07:32 +0400

    test improve: get only rooms user booked already

--- test/system/rooms_index_test_case.rb
+++ test/system/rooms_index_test_case.rb
@@ -1,13 +1,20 @@
 require "application_system_test_case"
 
 class RoomsIndexTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @rooms = rooms
+    @user = users(:one)
+    @rooms = @user.booked_rooms
   end
 
   test "viewing rooms" do
+    sign_in users(:one)
+
     visit rooms_path
+
     assert_selector "h1", text: "Rooms"
+
     @rooms.each do |room|
       assert_text room.name
       assert_text room.location
