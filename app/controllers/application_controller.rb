143506ef51d1a67a22077f03241dbf450f0ad872
point212 <point212@gmail.com>
2023-06-04 16:28:26 +0400

    made room booking works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  add_flash_types  :error, :success
   rescue_from ActiveRecord::RecordNotFound, with: -> { redirect_to '/404.html' }
 end
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,17 @@
-class RoomsController < ApplicationController
+class BookingsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def create
+    @booking = Booking.create booking_params.merge(user: current_user)
+    redirect_to room_path(params[:room_id]), flash: { error: 'Booking failed:' + @booking.errors.full_messages } if @booking.errors.any?
+
+    redirect_to rooms_path, flash: { success: 'Congrats! You have booked a room!' }
+  end
+
+  def booking_params
+    params.require(:booking).permit(:room_id, :from, :to)
   end
 end
