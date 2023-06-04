83562578089cd1db32082565affa64fc496322f3
point212 <point212@gmail.com>
2023-06-04 14:31:21 +0400

    show images on room page

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,6 @@
 # TODO: Add features to this model to implement the other TODO.
 class Room < ApplicationRecord
+  has_many_attached :images do |img|
+    img.variant :thumbnail, resize_to_limit: [400, 400]
+  end
 end
