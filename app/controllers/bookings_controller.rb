e4dd00e8f49b754a371650b45b8ab1d019a285c2
point212 <point212@gmail.com>
2023-06-05 13:44:13 +0400

    errors on checks added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,8 @@ class BookingsController < ApplicationController
   end
 
   def create
+    return redirect_to room_path(room_id), flash: { error: t('errors.booking_in_past') } if from < Date.today || to < Date.today
+    return redirect_to room_path(room_id), flash: { error: t('errors.to_must_be_above_from') } if to < from
     return redirect_to room_path(room_id), flash: { error: t('errors.already_booked') } unless can_book?
 
     Booking.create booking_params.merge(user: current_user)
@@ -24,9 +26,6 @@ class BookingsController < ApplicationController
   def to = booking_params[:to].to_date
 
   def can_book?
-    return false if from < Date.today || to < Date.today
-    return false if to < from
-
     Room.find_by!(id: room_id).can_book?(from, to)
   end
 end
