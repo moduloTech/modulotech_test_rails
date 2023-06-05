4b9c3f72386726161b0aae50efe87eb93d641239
point212 <point212@gmail.com>
2023-06-05 15:22:17 +0400

    add description field to the room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_094225) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_112119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_094225) do
     t.string "name"
     t.string "location"
     t.integer "price"
+    t.text "description"
     t.index ["location"], name: "index_rooms_on_location"
     t.index ["name"], name: "index_rooms_on_name"
   end
