df50c80b19b168083817871baa7ed52c8240df3f
point212 <point212@gmail.com>
2023-06-05 16:05:11 +0400

    rooms search (wip)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,12 @@
 class RoomsController < ApplicationController
   def index
     @rooms = Room.all
+
+    if params[:from].present? && params[:to].present?
+      @rooms = Room.booked(params[:from], params[:to])
+    end
+
+    @rooms = @rooms.where(location: params[:location]) if params[:location].present?
   end
 
   def show
