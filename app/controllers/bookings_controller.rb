f6cca622468d5378263e7f4374c7ec69aad2206e
point212 <point212@gmail.com>
2023-06-05 13:31:10 +0400

    booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,13 +5,21 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.create booking_params.merge(user: current_user)
-    redirect_to room_path(params[:room_id]), flash: { error: 'Booking failed:' + @booking.errors.full_messages } if @booking.errors.any?
+    return redirect_to room_path(booking_params[:room_id]), flash: { error: t('errors.already_booked') } unless can_book?
 
-    redirect_to rooms_path, flash: { success: 'Congrats! You have booked a room!' }
+    Booking.create booking_params.merge(user: current_user)
+    # redirect_to room_path(params[:room_id]), flash: { error: 'Booking failed:' + @booking.errors.full_messages } if @booking.errors.any?
+
+    redirect_to rooms_path, flash: { success: t('messages.booking_success') }
   end
+
+  private
 
   def booking_params
     params.require(:booking).permit(:room_id, :from, :to)
+  end
+
+  def can_book?
+    Room.find_by!(booking_params[:room_id]).can_book?(booking_params[:from], booking_params[:to])
   end
 end
